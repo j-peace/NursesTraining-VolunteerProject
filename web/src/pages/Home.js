63d6083fb230468431476c1d7146ba7d2ae943f8
tiagopazhs <tiagopaz.hs@gmail.com>
2023-01-29 19:52:19 -0300

    initial setup

---
+++ web/src/pages/Home.js
@@ -0,0 +1,14 @@
+import Typography from '@mui/material/Typography'
+function Home () {
+
+    return(
+        <>
+            <Typography variant="body1" color="initial">
+                Oi
+            </Typography>
+        </>
+    )
+
+}
+
+export default Home;
