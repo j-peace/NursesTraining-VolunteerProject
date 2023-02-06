509ef53274152408f88ea71864e6823a007688f4
tiagopazhs <tiagopaz.hs@gmail.com>
2023-02-05 21:03:10 -0300

    Add open clip board component

--- web/src/pages/Patient.js
+++ web/src/pages/Patient.js
@@ -11,17 +11,19 @@ import clip4 from '../assets/health-checkup.png'
 
 function Patient() {
 
-    const [showAlert, setShowAlert] = useState(true);
+    const [showAlert, setShowAlert] = useState(false);
+    const [showBag, setShowBag] = useState(true);
     const [showItems, setShowItems] = useState(false);
-    const [details, setDetails] = useState(false);
+    const [showRecords, setShowRecords] = useState(true);
+    const [showDetails, setShowDetails] = useState(false);
 
 
     return (
         <Box >
             <Box style={{ marginTop: '100px', backgroundColor: '#C2EBFF' }} height={'120vh'}>
-                <Alert variant="outlined" severity="error" hidden={showAlert}
+                <Alert variant="outlined" severity="error" hidden={!showAlert}
                     action={
-                        <Button color="inherit" size="small" onClick={() => setShowAlert(true)}>
+                        <Button color="inherit" size="small" onClick={() => setShowAlert(false)}>
                             UNDO
                         </Button>
                     }>
@@ -32,39 +34,57 @@ function Patient() {
                         xs={10} sm={8} md={6} lg={5} xl={5}>
                         <img
                             src={mrBarnes} style={{ width: '100%', borderRadius: 20, borderColor: '#f4ac4c', borderStyle: 'solid' }} />
-                        <Button variant="contained" disableElevation style={{ height: '2.8rem', marginTop: '-8rem', backgroundColor: '#005681' }} onClick={() => setShowAlert(false)} >
+                        <Button variant="contained" disableElevation style={{ height: '2.8rem', marginTop: '-8rem', backgroundColor: '#005681' }} onClick={() => setShowAlert(true)} >
                             Start appointment
                         </Button>
                     </Grid>
-                    {!showItems ?
+                    {showBag ?
                         <Grid item id="medicalKit"
                             xs={8} sm={6} md={4} lg={4} xl={4}>
-                            <Shake active={false}
+                            <Shake active={showAlert}
                                 h={0} v={45} r={2} dur={620} int={0.9} max={6} fixed={true} fixedStop={false} freez={false}>
-                                <img src={bag} style={{ width: '100%', cursor: "pointer" }} onClick={() => setShowItems(true)} />
+                                <img src={bag} style={{ width: '100%', cursor: "pointer" }} onClick={() => { setShowBag(false); setShowItems(true) }} />
                             </Shake>
-                        </Grid>
-                        :
+                        </Grid> : <></>}
+                    {showItems ?
                         <Grid item id="medicalKit"
-                            xs={11} sm={10} md={7} lg={5} xl={5} sx={{margin: 1}}>
-                            < Box backgroundColor={'#943c0c'} borderRadius={5} style={{ width: '100%', borderColor: '#f4ac4c', borderStyle: 'solid'}} onClick={() => setShowItems(false)} >
-                                <Grid container spacing={3} justifyContent="center" alignItems="center" pt={3} pb={3}>
-                                    <Grid item xs={5} sm={5}>
-                                        <img src={clip1} style={{width: '90%', cursor: 'pointer'}}/>
+                            xs={11} sm={10} md={7} lg={5} xl={5} sx={{ margin: 1 }}>
+                            {showRecords ?
+                                < Box backgroundColor={'#943c0c'} borderRadius={5} style={{ width: '100%', borderColor: '#f4ac4c', borderStyle: 'solid' }}>
+                                    <Grid container spacing={3} justifyContent="center" alignItems="center" pt={3} pb={3}>
+                                        <Grid item xs={5} sm={5}>
+                                            <Shake active={showAlert}
+                                                h={0} v={45} r={2} dur={620} int={0.9} max={6} fixed={true} fixedStop={false} freez={false}>
+                                                <img src={clip1} style={{ width: '90%', cursor: 'pointer' }} onClick={() => { setShowDetails(true); setShowRecords(false) }} />
+                                            </Shake>
+                                        </Grid>
+                                        <Grid item xs={5} sm={5}>
+                                            <Shake active={showAlert}
+                                                h={0} v={45} r={2} dur={620} int={0.9} max={6} fixed={true} fixedStop={false} freez={false}>
+                                                <img src={clip2} style={{ width: '100%', cursor: 'pointer' }} />
+                                            </Shake>
+                                        </Grid>
+                                        <Grid item xs={5} sm={5}>
+                                            <Shake active={showAlert}
+                                                h={0} v={45} r={2} dur={620} int={0.9} max={6} fixed={true} fixedStop={false} freez={false}>
+                                                <img src={clip3} style={{ width: '100%', cursor: 'pointer' }} />
+                                            </Shake>
+                                        </Grid>
+                                        <Grid item xs={5} sm={5}>
+                                            <Shake active={showAlert}
+                                                h={0} v={45} r={2} dur={620} int={0.9} max={6} fixed={true} fixedStop={false} freez={false}>
+                                                <img src={clip4} style={{ width: '96%', cursor: 'pointer' }} />
+                                            </Shake>
+                                        </Grid>
                                     </Grid>
-                                    <Grid item xs={5} sm={5}>
-                                        <img src={clip2} style={{width: '100%', cursor: 'pointer'}}/>
-                                    </Grid>
-                                    <Grid item xs={5} sm={5}>
-                                        <img src={clip3} style={{width: '100%', cursor: 'pointer'}}/>
-                                    </Grid>
-                                    <Grid item xs={5} sm={5}>
-                                        <img src={clip4} style={{width: '96%', cursor: 'pointer'}}/>
-                                    </Grid>
-                                </Grid>
-                            </Box>
-                        </Grid>
-                    }
+                                </Box>
+                                : []}
+                            {showDetails ?
+                                <Box backgroundColor={'red'} >
+                                    <Button onClick={() => { setShowDetails(false); setShowRecords(true) }} >close</Button>
+                                </Box>
+                                : []}
+                        </Grid> : []}
                 </Grid>
             </Box>
             <Paper id="navBar"
