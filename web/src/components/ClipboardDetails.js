5b1c4b5edbcdb8d724fdbab5d15fa914112f15c4
tiagopazhs <tiagopaz.hs@gmail.com>
2023-02-08 07:54:03 -0300

    Correct size on open clipboard details

--- web/src/components/ClipboardDetails.js
+++ web/src/components/ClipboardDetails.js
@@ -3,113 +3,44 @@ import { Box } from "@mui/system";
 
 export default function ClipboardDetails(props) {
     return (
-        <Box backgroundColor={'#f4ac4c'} sx={{ borderRadius: 7, height: '37vh', p: 2, display: 'grid', justifyContent: 'space-around', height: '30vw', minHeight: '35vh' }} >
+        <Box backgroundColor={'#f4ac4c'} sx={{ borderRadius: 7, height: '37vh', p: 2, display: 'grid', justifyContent: 'space-around', height: '30vw', minHeight: '45vh' }} >
             {props.recordType === 'personal' ?
                 <>
-                    <Typography variant="h4" fontFamily={'"Open Sans", sans-serif'} fontWeight={'bold'} sx={{ pt: 3 }} >
+                    <Typography variant="h4" fontFamily={'"Open Sans", sans-serif'} fontWeight={'bold'}  >
                         Personal Information
                     </Typography>
-                    <Grid style={{ display: 'flex' }}>
-                        <Typography variant="h6" fontFamily={'"Open Sans", sans-serif'} fontWeight={'bold'} sx={{ pr: 1 }} >
-                            Name:
-                        </Typography>
-                        <Typography variant="h6" fontFamily={'"Open Sans", sans-serif'}>
-                            Barners Jhonson
-                        </Typography>
-                    </Grid>
-                    <Grid style={{ display: 'flex' }}>
-                        <Typography variant="h6" fontFamily={'"Open Sans", sans-serif'} fontWeight={'bold'} sx={{ pr: 1 }} >
-                            Age:
-                        </Typography>
-                        <Typography variant="h6" fontFamily={'"Open Sans", sans-serif'}>
-                            70
-                        </Typography>
-                    </Grid>
-                    <Grid style={{ display: 'flex' }}>
-                        <Typography variant="h6" fontFamily={'"Open Sans", sans-serif'} fontWeight={'bold'} sx={{ pr: 1 }} >
-                            Gender:
-                        </Typography>
-                        <Typography variant="h6" fontFamily={'"Open Sans", sans-serif'}>
-                            Male
-                        </Typography>
-                    </Grid>
-                    <Grid style={{ display: 'flex' }}>
-                        <Typography variant="h6" fontFamily={'"Open Sans", sans-serif'} fontWeight={'bold'} sx={{ pr: 1 }} >
-                            Ethnicities:
-                        </Typography>
-                        <Typography variant="h6" fontFamily={'"Open Sans", sans-serif'}>
-                            African American
-                        </Typography>
-                    </Grid>
+                    <Typography variant="h6" fontFamily={'"Open Sans", sans-serif'} marginRight={1}><Box fontWeight={'bold'} display='inline'>Name:</Box>  Barners Jhonson </Typography>
+                    <Typography variant="h6" fontFamily={'"Open Sans", sans-serif'} marginRight={1}><Box fontWeight={'bold'} display='inline'>Age:</Box> 70 </Typography>
+                    <Typography variant="h6" fontFamily={'"Open Sans", sans-serif'} marginRight={1}><Box fontWeight={'bold'} display='inline'>Gender:</Box> Male </Typography>
+                    <Typography variant="h6" fontFamily={'"Open Sans", sans-serif'} marginRight={1}><Box fontWeight={'bold'} display='inline'>Ethnicities:</Box>  African American </Typography>
                 </>
                 : []}
             {props.recordType === 'diagnosis' ?
                 <>
-                    <Typography variant="h4" fontFamily={'"Open Sans", sans-serif'} fontWeight={'bold'} sx={{ pt: 3 }} >
-                        Diagnosis
-                    </Typography>
-                    <Typography variant="h6" fontFamily={'"Open Sans", sans-serif'}>
-                        Healing peripheral vascular ulcer on his right foot.
-                    </Typography>
-                    <Typography variant="body2" fontFamily={'"Open Sans", sans-serif'}>
-                        Evidence of infection, clotting problems, abnormal liver or kidney function, impaired oxygen availability, electrolyte imbalances.
-                    </Typography>
-                    <Grid style={{ display: 'flex' }}>
-                        <Typography variant="h6" fontFamily={'"Open Sans", sans-serif'} color='red' fontWeight={'bold'}sx={{ pr: 1 }} >
-                        Sepsis: 
-                        </Typography>
-                    <Typography variant="h6" fontFamily={'"Open Sans", sans-serif'} color='red'>
-                        Reagent
-                    </Typography>
-                    </Grid>
+                    <Typography variant="h4" fontFamily={'"Open Sans", sans-serif'} fontWeight={'bold'}>
+                        Diagnosis</Typography>
+                    <Typography variant="h6" fontFamily={'"Open Sans", sans-serif'}>Healing peripheral vascular ulcer on his right foot.</Typography>
+                    <Typography variant="body2" fontFamily={'"Open Sans", sans-serif'}>Evidence of infection, clotting problems, abnormal liver or kidney function, impaired oxygen availability, electrolyte imbalances.</Typography>
+                    <Typography variant="h6" color='red' fontFamily={'"Open Sans", sans-serif'} marginRight={1}><Box fontWeight={'bold'} display='inline'>Sepsis:</Box> Reagent</Typography>
                 </>
                 : []}
             {props.recordType === 'prescription' ?
                 <>
-                    <Typography variant="h4" fontFamily={'"Open Sans", sans-serif'} fontWeight={'bold'} sx={{ pt: 3 }} >
-                        Doctor's prescription
-                    </Typography>
-                    <Grid style={{ display: 'flex' }}>
-                        <Typography variant="h6" fontFamily={'"Open Sans", sans-serif'} fontWeight={'bold'} sx={{ pr: 1 }} >
-                            Antibiotics:
-                        </Typography>
-                        <Typography variant="h6" fontFamily={'"Open Sans", sans-serif'}>
-                            Follow the medical recipe.
-                        </Typography>
-                    </Grid>
-                    <Grid style={{ display: 'flex' }}>
-                        <Typography variant="h6" fontFamily={'"Open Sans", sans-serif'} fontWeight={'bold'} sx={{ pr: 1 }} >
-                            Bandages:
-                        </Typography>
-                        <Typography variant="h6" fontFamily={'"Open Sans", sans-serif'}>
-                            Wound healing peripheral vascular ulcer on right foot: Wet-to-dry dressings two times a day.
-                        </Typography>
-                    </Grid>
+                    <Typography variant="h4" fontFamily={'"Open Sans", sans-serif'} fontWeight={'bold'}>
+                        Doctor's prescription</Typography>                    
+                    <Typography variant="h6" fontFamily={'"Open Sans", sans-serif'} marginRight={1}><Box fontWeight={'bold'} display='inline'>Antibiotics:</Box> Follow the medical recipe. </Typography>
+                    <Typography variant="h6" fontFamily={'"Open Sans", sans-serif'} marginRight={1}><Box fontWeight={'bold'} display='inline'>Bandages:</Box> Wound healing peripheral vascular ulcer on right foot: Wet-to-dry dressings two times a day.</Typography>
                 </>
                 : []}
-                {props.recordType === 'summary' ?
-                    <>
-                        <Typography variant="h4" fontFamily={'"Open Sans", sans-serif'} fontWeight={'bold'} sx={{ pt: 3 }} >
+            {props.recordType === 'summary' ?
+                <>
+                    <Typography variant="h4" fontFamily={'"Open Sans", sans-serif'} fontWeight={'bold'}>
                         Discharge summary
-                        </Typography>
-                        <Grid style={{ display: 'flex' }}>
-                            <Typography variant="h6" fontFamily={'"Open Sans", sans-serif'} fontWeight={'bold'} sx={{ pr: 1 }} >
-                                Description:
-                            </Typography>
-                            <Typography variant="h6" fontFamily={'"Open Sans", sans-serif'}>
-                            Mr. Banks must needs continue to care for his right foot at home twice a day with wet-dry dressings.
-                            </Typography>
-                        </Grid>
-                        <Grid style={{ display: 'flex' }}>
-                            <Typography variant="h6" fontFamily={'"Open Sans", sans-serif'} fontWeight={'bold'} sx={{ pr: 1 }} >
-                                Status:
-                            </Typography>
-                            <Typography variant="h6" fontFamily={'"Open Sans", sans-serif'} fontWeight={'bold'} color={'green'}>
-                            Discharged
-                            </Typography>
-                        </Grid>
-                    </>
-                    : []}
+                    </Typography>
+                    <Typography variant="h6" fontFamily={'"Open Sans", sans-serif'} marginRight={1}><Box fontWeight={'bold'} display='inline'>Description:</Box> Mr. Banks must needs continue to care for his right foot at home twice a day with wet-dry dressings.</Typography>
+                    <Typography variant="h6" fontFamily={'"Open Sans", sans-serif'} color={'green'} fontWeight={'bold'} marginRight={1}><Box display='inline' color={'black'}>Status:</Box> Discharged</Typography>
+                </>
+                : []}
             <Grid container justifyContent="center">
                 <Grid item xs={8}>
                     <Button onClick={props.action}
