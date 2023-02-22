910e41013860d81a7f8f89c43d848dd44e4ac797
tiagopazhs <tiagopaz.hs@gmail.com>
2023-02-22 09:44:08 -0300

    PatientPage: Add boxshadow to the butons

--- web/src/components/ClipBoardImg.js
+++ web/src/components/ClipBoardImg.js
@@ -9,7 +9,7 @@ export default function ClipBoardImg(props) {
                 h={0} v={45} r={2} dur={620} int={0.9} max={6} fixed={true} fixedStop={false} freez={false}>
                 <img src={props.clipImg} style={{ width: props.clipWidth, cursor: 'pointer' }} onClick={ props.actionClip} />
                 <img src={doneButton} style={{ width: '30%', marginBottom: '-70%', marginLeft: '-90%'}} hidden={props.checked} />
-                <Button variant="contained" style={{width: '90%', height:'3rem',  marginBottom: '-70%', marginLeft: '-90%', boxShadow: '30 0 30 red',  backgroundColor: '#005681' }}  > {props.clipTitle}</Button>
+                <Button variant="contained" style={{width: '90%', height:'3.2rem',  marginBottom: '-70%', marginLeft: '-90%', boxShadow: '0px 5px 10px 0px rgba(0, 0, 0, 0.5)',  backgroundColor: '#005681' }}  > {props.clipTitle}</Button>
             </Shake>
         </Grid>
     )
