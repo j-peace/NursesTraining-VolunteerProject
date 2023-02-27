79f2eb2559089355d7c0bb8b15856e5a0b2e3217
tiagopazhs <tiagopaz.hs@gmail.com>
2023-02-27 09:14:09 -0300

    Add HashRouter

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 
 import Home from './pages/Home'
 import PatientsList from './pages/PatientsList'
@@ -8,13 +8,13 @@ import Patient from './pages/Patient'
 
 function App () {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />}  />
         <Route path="/PatientsList" element={<PatientsList />}/>
         <Route path="/Patient" element={<Patient />}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
