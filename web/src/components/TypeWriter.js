0246d8f048ef40d04b20a61218f4ca2d682f79b4
tiagopazhs <tiagopaz.hs@gmail.com>
2023-03-01 20:57:53 -0300

    revision of typeWriter useffect

--- web/src/components/TypeWriter.js
+++ web/src/components/TypeWriter.js
@@ -5,32 +5,44 @@ import React, { useState, useEffect } from 'react';
 export default function TypeWriter(props, { typingSpeed = 40 }) {
 
   const text = props.text
+  const [writedText, setWritedText] = useState('...')
 
-  const [typedText, setTypedText] = useState('Oi');
+  // if (props.end) { console.log(props.end); setWriteddText(text) }
+
+
+  // function type() {
+  //   let i = 0;
+  //   if (i >= text.length) return;
+  //   else {
+  //     setWritedText(text.slice(0, i));
+  //     console.log('here', i, writedText)
+  //     i++
+  //     return
+  //   }
+  // }
 
   useEffect(() => {
-    if (props.end) { setTypedText(text) }
-    else {
-      let i = 0;
-      const type = () => {
-        setTypedText(text.slice(0, i));
-        if (i < text.length) {
-          setTimeout(() => {
-            i++;
-            type();
-          }, typingSpeed);
-        }
-      };
-      type();
-    }
-  }, [text, typingSpeed]);
+    let i = 0;
+    function type() {
+      if (props.end) { setWritedText(text) }
+      setWritedText(text.slice(0, i));
+      if (i < text.length) {
+        setTimeout(() => { i++; type() }, typingSpeed);
+      }
+    };
+    type();
+  }, [props.hidden]);
+
+  useEffect(() => {
+    if (props.end) { setWritedText(text);  console.log('hereeee')}
+  }, [props.end]);
 
   return (
     <Typography variant="h6" fontFamily={'"Open Sans", sans-serif'} marginRight={1} hidden={props.hidden}>
       <Box fontWeight={'bold'} display='inline' mr={1}>
         {props.boldText}
       </Box>
-      {typedText}
+      {writedText}
     </Typography>
   )
 };
