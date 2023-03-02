1f7d58fa23cd36f5560e1944579b61da1f466d60
tiagopazhs <tiagopaz.hs@gmail.com>
2023-03-01 23:16:14 -0300

    revision of quention and remove unnessary code

--- web/src/components/Question.js
+++ web/src/components/Question.js
@@ -8,28 +8,18 @@ import { Grid, Typography } from "@mui/material";
 import { green } from "@mui/material/colors";
 
 
+
 export default function Question(props) {
 
     return (
-        <Box style={{ backgroundColor: "white" }} sx={{ boxShadow: 3, borderRadius: 2, "&:hover": { boxShadow: 6, transform: "translateY(-2px)" } }} hidden={props.hidden}>
+        <Box style={{ backgroundColor: "white" }} sx={{ borderStyle: 'solid', borderColor: props.border, boxShadow: 3, borderRadius: 2, "&:hover": { boxShadow: 6, transform: "translateY(-2px)" } }} hidden={props.hidden}>
             <ListItem direction="row" justifyContent="center" alignItems="center" sx={{ boxShadow: 3, borderRadius: 2, "&:hover": { boxShadow: 6, transform: "translateY(-2px)", backgroundColor: '#F5F5F5', cursor: 'pointer' } }} >
                 <ListItemAvatar>
-                    {props.patientName ? <Avatar alt={props.patientName} sx={{ bgcolor: green[800] }}>{props.patientName}</Avatar> : <Avatar alt={props.patientName} >{props.patientName}</Avatar>}
+                    <Avatar alt={props.alternative} sx={{ bgcolor: 'gray' }}>{props.alternative}</Avatar>
                 </ListItemAvatar>
-                <Grid
-                    container
-                    direction="row"
-                    justifyContent="center"
-                    alignItems="center"
-                >
-                    <Grid item xs={12} xl={6}>
-                        <ListItemText>
-                            <Typography variant="body2">
-                                {props.text}
-                            </Typography >
-                        </ListItemText>
-                    </Grid>
-                </Grid>
+                <Typography variant="body2">
+                    {props.text}
+                </Typography >
             </ListItem>
         </Box>
     );
